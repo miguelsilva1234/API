3c8ede5c2730359d74f9702a3c6f18513a635df0
miguelsilva1234 <161628496+miguelsilva1234@users.noreply.github.com>
2026-04-14 16:11:49 -0300

    Add files via upload

--- api.php
+++ api.php
@@ -1,32 +1,63 @@
 <?php
     //cabeçalho
-    header("Content-Type: application/json"); // Define o tipo de resposta
+    header("Content-Type: application/json; charset=UTF-8"); // Define o tipo de resposta
 
     $metodo = $_SERVER['REQUEST_METHOD'];
+    //echo "metodo de requisicao:" . $metodo;
 
-    //echo "Metodo da requisicao: " ,$metodo;
+    // recupera o arquivo json na mesma pasta do projeto
+    $arquivo = 'usuarios.json';
+
+    // verifica se o arquivo existe, se nao existir cria um com array vazio
+    if (!file_exists($arquivo)) {
+        file_put_contents($arquivo, json_encode([], JSON_PRETTY_PRINT | JSON_UNESCAPED_UNICODE));
+    }
+
+    $usuarios = json_decode(file_get_contents($arquivo), true);
+    //$usuarios = [
+      //  ["id" => 1, "nome" => "Maria Souza", "email" => "maria@gmail.com"],
+        //["id" => 2, "nome" => "Joao Silva", "email" => "joao@gmail.com"]
+    //];
 
     switch ($metodo) {
         case 'GET':
-            echo "AQUI AÇÕES DO METODO GET;";
+            //echo "AQUI AÇÕES DO METODO GET;";
+            echo json_encode($usuarios);
             break;
 
         case 'POST':
-            echo "AQUI AÇÕES DO METODO POST;";
+            //echo "AQUI AÇÕES DO METODO POST;";
+            $dados = json_decode(file_get_contents("php://input"), true);
+            //print_r($dados);
+            $novoUsuario = [
+                "id" => $dados["id"],
+                "nome" => $dados["nome"],
+                "email" => $dados["email"]
+            ];
+
+            // Adiciona o novo usuario ao array existente
+            array_push($usuarios, $novoUsuario);
+            echo json_encode('Uuario inserido com sucesso!');
+            print_r($usuarios);
+
+            break;
+
+        case 'PUT':
+            echo "AQUI AÇÕES DO METODO PUT;";
+            break;
+
+        case 'DELETE':
+            echo "AQUI AÇÕES DO METODO DELETE;";
             break;
         
         default:
             echo "METODO NAO ENCONTRADO";
             break;
     }
-    //Conteudo
-    //$usuarios = [
-    //    ["id" => 1, "nome" => "Maria Souza", "email" => "maria@gmail.com"],
-    //   ["id" => 2, "nome" => "Joao Silva", "email" => "joao@gmail.com"]
-    //];
+    
 
     // Converte para JSON e retorna
-    //echo json_encode($usuarios);
+    
 
 ?>
     
